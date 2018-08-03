8477f4eae25431068105440a9c1d57d483edd27e
Sav Younts <savyounts@gmail.com>
2018-08-03 19:42:35 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,8 +6,8 @@ def reverse_each_word(sentence)
     #reverse = word.reverse 
     #reverse_array << reverse
     
-    reverse_array = array.collect do |word|
-      word.reverse
+  reverse_array = array.collect do |word|
+   word.reverse
   end
   reverse_array.join(" ")
 end
